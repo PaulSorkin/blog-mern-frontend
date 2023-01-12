deaa42acec605f7ea3f53aedf04dbd539aad36ab
ÑPaul Sorkin <sorkinpavel@mail.ru>
2023-01-13 01:20:20 +0200

    feat: update post

--- src/pages/AddPost/index.jsx
+++ src/pages/AddPost/index.jsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useMemo, useRef, useState } from "react";
+import React, { useCallback, useEffect, useMemo, useRef, useState } from "react";
 import TextField from '@mui/material/TextField';
 import Paper from '@mui/material/Paper';
 import Button from '@mui/material/Button';
@@ -8,10 +8,11 @@ import 'easymde/dist/easymde.min.css';
 import styles from './AddPost.module.scss';
 import { useSelector } from "react-redux";
 import { selectIsAuth } from "../../redux/slices/auth";
-import { Navigate, useNavigate } from "react-router-dom";
+import { Navigate, useNavigate, useParams } from "react-router-dom";
 import axios from "../../axios";
 
 export const AddPost = () => {
+  const { id } = useParams();
   const navigate = useNavigate();
   const isAuth = useSelector(selectIsAuth);
   const [isLoading, setIsLoading] = useState(false);
@@ -21,6 +22,8 @@ export const AddPost = () => {
   const [imageUrl, setImageUrl] = useState('');
 
   const inputFileRef = useRef(null);
+
+  const isEditing = Boolean(id);
 
   const handleChangeFile = async (event) => {
     try {
@@ -51,14 +54,32 @@ export const AddPost = () => {
         tags: Array.from(tags.split(',')),
         text
       };
-      const { data } = await axios.post('/posts', fields);
-      const id = data._id;
-      navigate(`/posts/${id}`);
+      const { data } = isEditing ?
+        (await axios.patch(`/posts/${id}`, fields)) :
+        (await axios.post('/posts', fields));
+
+      const _id = isEditing ? id : data._id;
+      navigate(`/posts/${_id}`);
     } catch (err) {
       console.warn(err);
       alert('Failed to create a post');
     }
   };
+
+  useEffect(() => {
+    if (id) {
+      axios.get(`/posts/${id}`)
+        .then(({ data }) => {
+        setTitle(data.title);
+        setText(data.text);
+        setImageUrl(data.imageUrl);
+        setTags(data.tags.join(','));
+      }).catch(err => {
+        console.warn(err);
+        alert('Failed to get the post')
+      })
+    }
+  }, []);
 
 
   //useCallback is mandatory to use with SimpleMDE
@@ -124,7 +145,7 @@ export const AddPost = () => {
                  options={options} />
       <div className={styles.buttons}>
         <Button size="large" variant="contained" onClick={onSubmit}>
-          Post now
+          {isEditing ? 'Save changes' : 'Post now'}
         </Button>
         <a href="/">
           <Button size="large">Cancel</Button>
